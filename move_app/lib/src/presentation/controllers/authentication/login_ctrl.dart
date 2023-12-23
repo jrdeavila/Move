f653c9f4876ad25b1c6dd70666af910a979c311e
Jose De avila <jrdeavila@unicesar.edu.co>
2023-12-22 19:18:57 -0500

    Update Version

--- move_app/lib/src/presentation/controllers/authentication/login_ctrl.dart
+++ move_app/lib/src/presentation/controllers/authentication/login_ctrl.dart
@@ -1,7 +1,28 @@
 import 'package:move_app/lib.dart';
 
 class LoginCtrl extends GetxController {
+  final RxBool _loading = false.obs;
+
+  @override
+  void onReady() {
+    super.onReady();
+
+    ever(_loading, _showLoading);
+  }
+
+  void _showLoading(show) {
+    if (show) {
+      Get.offAllNamed(MainRoutes.loading);
+    }
+  }
+
   void loginWithGoogle() {
-    getIt<ILoginWithGoogleUseCase>().loginWithGoogle();
+    _loading.value = true;
+    getIt<ILoginWithGoogleUseCase>()
+        .loginWithGoogle()
+        .onError((error, stackTrace) {
+      _loading.value = false;
+      Get.snackbar("Error", error.toString());
+    }).whenComplete(() => _loading.value = false);
   }
 }
--- move_app/lib/src/presentation/controllers/authentication/register_ctrl.dart
+++ move_app/lib/src/presentation/controllers/authentication/register_ctrl.dart
@@ -4,6 +4,7 @@ class RegisterCtrl extends GetxController {
   final RxString _firstname = ''.obs;
   final RxString _lastname = ''.obs;
   final RxString _phone = ''.obs;
+  final RxBool _loading = false.obs;
 
   String get firstname => _firstname.value.trim();
   String get lastname => _lastname.value.trim();
@@ -21,8 +22,21 @@ class RegisterCtrl extends GetxController {
     _phone.value = text;
   }
 
+  @override
+  void onReady() {
+    super.onReady();
+    ever(_loading, _showLoading);
+  }
+
+  void _showLoading(show) {
+    if (show) {
+      Get.offAllNamed(MainRoutes.loading);
+    }
+  }
+
   void register() {
     final useCase = getIt<IRegisterUseCase>();
+    _loading.value = true;
     useCase
         .register(
           RegisterRequest(
@@ -33,6 +47,12 @@ class RegisterCtrl extends GetxController {
         )
         .then(
           (value) => Get.find<SessionCtrl>().onRegisterSuccess(value),
-        );
+        )
+        .onError((error, stackTrace) {
+      _loading.value = false;
+      Get.snackbar("Error", error.toString());
+    }).whenComplete(() {
+      _loading.value = false;
+    });
   }
 }
